3076f1e325450c66077ec45abf1e3e08ba40205f
Dmitri Tchemski <tchemski@gmail.com>
2019-03-07 12:35:23 +0300

    chore: add Test model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_053330) do
+ActiveRecord::Schema.define(version: 2019_03_07_092554) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "title"
+    t.integer "level"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
 end
