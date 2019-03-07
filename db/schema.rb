9f20b7ce0df60f4bf04d3c87119cf891f67c1a73
Dmitri Tchemski <tchemski@gmail.com>
2019-03-07 15:20:13 +0300

    chore: add Answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_095017) do
+ActiveRecord::Schema.define(version: 2019_03_07_121738) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.boolean "correct"
+    t.integer "question_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
