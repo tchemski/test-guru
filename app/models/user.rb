7b7ccdec2b53ae13da91870f99de21fd6f6460da
Dmitri Tchemski <tchemski@gmail.com>
2019-03-13 12:52:59 +0300

    feat: add tests method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def tests(test_level = nil)
+    t = Test.joins('INNER JOIN tests_users ON tests_users.test_id = tests.id')
+            .where('tests_users.user_id = ?', id)
+            .distinct
+    t = t.where('level=?', test_level) if test_level
+    t
+  end
 end
