09a9e1a73c9fb1f69bab066541527d194df888bd
Dmitri Tchemski <tchemski@gmail.com>
2019-03-13 16:10:57 +0300

    fix: remove the optional parameter from the User#tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
-  def tests(test_level = nil)
-    t = Test.joins('INNER JOIN tests_users ON tests_users.test_id = tests.id')
-            .where('tests_users.user_id = ?', id)
-            .distinct
-    t = t.where('level=?', test_level) if test_level
-    t
+  def tests(test_level)
+    Test.joins('INNER JOIN tests_users ON tests_users.test_id = tests.id')
+        .where('tests_users.user_id = ? AND level=?', id, test_level)
+        .distinct
   end
 end
