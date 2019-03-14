c1b9af6eea84f8e03c12928de22658bdb2bdabc6
Dmitri Tchemski <tchemski@gmail.com>
2019-03-14 23:59:07 +0300

    feat: add author to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,6 +3,7 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :tests_users
   has_many :users, through: :tests_users
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
   def self.titles_by_category_title(category_title)
     self.joins(:category)
