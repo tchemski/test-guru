99a7f998d87cca10b5607613b1226d129f09c64c
Dmitri Tchemski <tchemski@gmail.com>
2019-03-15 15:40:26 +0300

    feat: add default scope to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   has_many :tests, dependent: :destroy
   has_many :categories_users
   has_many :users, through: :categories_users
+
+  default_scope { order(:title) }
 end
