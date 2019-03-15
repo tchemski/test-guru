9ec44e5dcb4a9a2d149553362132d5288588423f
Dmitri Tchemski <tchemski@gmail.com>
2019-03-16 00:22:20 +0300

    fix: repaire migration

--- db/migrate/20190307092554_create_tests.rb
+++ db/migrate/20190307092554_create_tests.rb
@@ -5,9 +5,6 @@ class CreateTests < ActiveRecord::Migration[5.2]
       t.integer :level, default: 0
       t.references :category, foreign_key: true
       t.references :user, foreign_key: true
-      t.index [:title, :level],
-              unique: true,
-              name: 'index_tests_title_and_level'
 
       t.timestamps
     end
