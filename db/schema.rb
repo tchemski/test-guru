0439c7c89e9bae297b22bfc3ee1d38b9a15d07fa
Dmitri Tchemski <tchemski@gmail.com>
2019-03-16 08:45:37 +0300

    feat: add a unique index on title and level in tests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_082005) do
+ActiveRecord::Schema.define(version: 2019_03_16_050847) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_082005) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
-    t.index ["title", "level"], name: "index_tests_title_and_level", unique: true
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
