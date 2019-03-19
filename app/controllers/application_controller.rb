3a6d64a807b462c943b7afe6e73f2b2bdd87e3d9
Dmitri Tchemski <tchemski@gmail.com>
2019-03-19 14:30:14 +0300

    feat: add tests_controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def user_auth
+    # TODO: пока нет аутентификации первого попавшегося пользователя
+    @user = User.take
+  end
 end
