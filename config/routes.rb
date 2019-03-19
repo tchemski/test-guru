9383bacd49a5ae756f4248973de11081474f00c1
Dmitri Tchemski <tchemski@gmail.com>
2019-03-19 14:23:15 +0300

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tests#index'
+
+  resources :tests, param: :test_id do
+
+    get :beginner, on: :collection
+
+    member do
+      post :start
+      resources :questions, shallow: true, param: :question_id
+    end
+  end
 end
