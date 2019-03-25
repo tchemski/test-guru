39654ec5cd7af4f01c1eca92dcf232eca56047da
Dmitri Tchemski <tchemski@gmail.com>
2019-03-26 00:17:45 +0300

    fix:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def now_year
-    DateTime.now.year
+  def current_year
+    DateTime.current.year
   end
 
-  def github_url(author, repo)
-    "https://github.com/#{author}/#{repo}"
+  def github_link(author, repo)
+    link_to("#{author}/#{repo}", "https://github.com/#{author}/#{repo}", target: '_blank')
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,13 +1,12 @@
 module QuestionsHelper
-  BEGINING_TEXT = {
-    new: 'Create New',
-    edit: 'Edit'
+  HEADER_TEXT = {
+    new: 'Create New %s Question',
+    edit: 'Edit %s Question'
   }.freeze
 
-  ENDING_TEXT = 'Question'.freeze
-
   # Create New <Test Title> Question, а в форме редактирования Edit <Test Title> Question
-  def header_text(action_name, test)
-    "#{BEGINING_TEXT[action_name.to_sym]} #{test.title} #{ENDING_TEXT}"
+  def header_text(question)
+    header = question.new_record? ? HEADER_TEXT[:new] : HEADER_TEXT[:edit]
+    format(header, question.test.title)
   end
 end
